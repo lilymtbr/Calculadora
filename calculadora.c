520d48d80c0d72234a2738bb0c717cc63d260b89
lilymtbr <lm@cesar.school>
2024-08-30 09:36:51 -0300

    Questão 1: a

--- calculadora.c
+++ calculadora.c
@@ -2,34 +2,49 @@
 #include <stdlib.h>
 #include <string.h>
 
-int base_10_para_base2 (int numero) {
+void base_10_para_base2 (int numero) {
+    int vetor[100];
+    int i = 0;
     while (numero>0) {
-        
+        vetor[i] = numero % 2;
+        numero=numero/2;
+        i++;
     }
+
+    for (i = i - 1; i >= 0; i--) {
+        printf("%d", vetor[i]);
+    }
+    printf("\n");
 }
 
-int base_10_para_base8 () {
-
+int base_10_para_base8 (int numero) {
+    printf("Nada ainda.\n");
+    return 0;
 }
 
-int base_10_para_base16 () {
-
+int base_10_para_base16 (int numero) {
+    printf("Nada ainda.\n");
+    return 0;
 }
 
-int base_10_para_BCD () {
-
+int base_10_para_BCD (int numero) {
+    printf("Nada ainda.\n");
+    return 0;
 }
 
-int base_10_para_base_sinal_16bits () {
-
+int base_10_para_base_sinal_16bits (int numero) {
+    printf("Nada ainda.\n");
+    return 0;
 }
 
-double real_para_float () {
-
+double real_para_float (int numero) {
+    printf("Nada ainda.\n");
+    return 0;
 }
 
-double real_para_double () {
-
+double real_para_double (int numero2) {
+    printf("Nada ainda.\n");
+    return 0;
 }
 
 int main(void) {
@@ -39,37 +54,39 @@ int main(void) {
 
     while (continuar == 1) {
         printf("Digite que operação você deseja realizar na calculadora: \n1- Para converter um número inteiro de base 10 para base 2\n2- Para converter um número inteiro de base 10 para base 8\n3- Para converter um número inteiro de base 10 para base 16\n4- Para converter um número inteiro de base 10 para código BCD\n5- Para converter um número inteiro de base 10 para base com sinal com 16 bits (complemento a 2)\n6- Para converter real em decimal para float e double\n7- Para encerrar o programa\n ");
-        
+        scanf("%d", &operacao);
+
         switch(operacao) {
             case 1:
-                printf("Digite um número inteiro: ");
-                scanf("%d", numero);
+                printf("Digite um número inteiro: \n");
+                scanf("%d", &numero);
+                base_10_para_base2 (numero);
                 break;
 
             case 2:
-                printf("Digite um número inteiro: ");
-                scanf("%d", numero);
+                printf("Digite um número inteiro: \n");
+                scanf("%d", &numero);
                 break;
 
             case 3:
-                printf("Digite um número inteiro: ");
-                scanf("%d", numero);
+                printf("Digite um número inteiro: \n");
+                scanf("%d", &numero);
                 break;
 
             case 4:
-                printf("Digite um número inteiro: ");
-                scanf("%d", numero);
+                printf("Digite um número inteiro: \n");
+                scanf("%d", &numero);
                 break;
 
             case 5:
-                printf("Digite um número inteiro: ");
-                scanf("%d", numero);
+                printf("Digite um número inteiro: \n");
+                scanf("%d", &numero);
                 break;
 
             case 6:
-                printf("Digite um número inteiro: ");
-                scanf("%d", numero);
-                break:
+                printf("Digite um número real: \n");
+                scanf("%lf", &numero2);
+                break;
             
             case 7:
                 printf("Você encerrou o programa!\n");
